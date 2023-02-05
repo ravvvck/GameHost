aec6a6455010d5f1e1fd2c63e3016218b3186b08
rafalkwiatek1 <rafal.kwiatek@edu.uekat.pl>
2023-02-05 18:00:23 +0100

    Add session mapping

--- GameHost.Api/Mapping/AuthenticationMappingConfig.cs
+++ GameHost.Api/Mapping/AuthenticationMappingConfig.cs
@@ -3,20 +3,25 @@ using GameHost.Application.Authentication.Commands.Register;
 using GameHost.Application.Authentication.Queries;
 using GameHost.Application.Common.Interfaces.Services;
 using GameHost.Contracts.Authentication;
+using GameHost.Domain.Common.Models;
 using Microsoft.Extensions.Logging;
 
 namespace GameHost.Api.Mapping
 {
     public class AuthenticationMappingConfig : Profile
-    {
+    { 
         public AuthenticationMappingConfig() 
         {
             CreateMap<AuthenticationResult, AuthenticationResponse>()
-                .ForMember(result => result.FirstName, r => r.MapFrom(response => response.user.FirstName))
-                .ForMember(result => result.LastName, r => r.MapFrom(response => response.user.LastName))
-                .ForMember(result => result.Email, r => r.MapFrom(response => response.user.Email));
+                .ForMember(result => result.Id, r => r.MapFrom(response => response.User.Id.Value))
+                .ForMember(result => result.FirstName, r => r.MapFrom(response => response.User.FirstName))
+                .ForMember(result => result.LastName, r => r.MapFrom(response => response.User.LastName))
+                .ForMember(result => result.Email, r => r.MapFrom(response => response.User.Email))
+                .ForMember(result => result.Token, r => r.MapFrom(response => response.Token));
+
             CreateMap<RegisterRequest, RegisterCommand>();
             CreateMap<LoginRequest, LoginQuery>();
+            
         }
     }
 }
---
+++ GameHost.Api/Mapping/SessionMappingConfig.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using GameHost.Application.Sessions.Commands.CreateSession;
+using GameHost.Contracts.Sessions;
+using GameHost.Domain.Session;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace GameHost.Api.Mapping
+{
+    public class SessionMappingConfig : Profile
+    {
+        public SessionMappingConfig()
+        {
+            CreateMap<Session, SessionResponse>();
+            CreateMap<Game, GameCommand>();
+            CreateMap<(CreateSessionRequest request, string HostId), CreateSessionCommand>()
+                .ForMember(dest => dest.HostId, r => r.MapFrom(src => src.HostId))
+            //.ForAllMembers(r => r.MapFrom(src => src.request)); 
+            .ForMember(dest => dest.Name, r => r.MapFrom(src => src.request.Name))
+            .ForMember(dest => dest.Description, r => r.MapFrom(src => src.request.Description))
+            .ForMember(dest => dest.Address, r => r.MapFrom(src => src.request.Address))
+            .ForMember(dest => dest.Games, r => r.MapFrom(src => src.request.Games))
+            .ForMember(dest => dest.Date, r => r.MapFrom(src => src.request.Date));
+
+
+            CreateMap<Session, SessionResponse>();
+           
+
+        }
+    }
+}
