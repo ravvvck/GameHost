44b938616bf0b50509d6aa85af272db0d595e7b6
Rafał Kwiatek <rafal.kwiatek@edu.uekat.pl>
2023-02-27 22:07:30 +0100

    Add update session command

---
+++ GameHost.Application/Sessions/Commands/UpdateSession/UpdateSessionCommand.cs
@@ -0,0 +1,33 @@
+﻿using GameHost.Contracts.Sessions;
+using GameHost.Domain.Common.ValueObjects;
+using GameHost.Domain.Sessions;
+using GameHost.Domain.Sessions.ValueObjects;
+using GameHost.Domain.Users.ValueObjects;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHost.Application.Sessions.Commands.UpdateSession
+{
+    public class UpdateSessionCommand : IRequest<SessionResponse>
+    {
+        public SessionId SessionId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public DateTime Date { get; set; }
+        public List<GameCommand> Games { get; set; }
+    }
+
+    public class GameCommand
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string InfoURL { get; set; }
+    }
+    
+    }
+
---
+++ GameHost.Application/Sessions/Commands/UpdateSession/UpdateSessionCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿//using GameHost.Application.Authentication.Authorization;
+//using GameHost.Application.Common.Interfaces.Persistence;
+//using GameHost.Application.Exceptions;
+//using GameHost.Application.Sessions.Commands.DeleteSession;
+//using GameHost.Contracts.Sessions;
+//using MediatR;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace GameHost.Application.Sessions.Commands.UpdateSession
+//{
+//    internal class UpdateSessionCommandHandler : IRequestHandler<UpdateSessionCommand, SessionResponse>
+//    {
+//        private readonly ISessionRepository sessionRepository;
+//        private readonly IHttpContextAccessor httpContextAccessor;
+//        private readonly IAuthorizationService authorizationService;
+
+//        public UpdateSessionCommandHandler(ISessionRepository sessionRepository, IHttpContextAccessor httpContextAccessor, IAuthorizationService authorizationService)
+//        {
+//            this.sessionRepository = sessionRepository;
+//            this.httpContextAccessor = httpContextAccessor;
+//            this.authorizationService = authorizationService;
+//        }
+//        public async Task<SessionResponse> Handle(UpdateSessionCommand request, CancellationToken cancellationToken)
+//        {
+//            var user = httpContextAccessor.HttpContext.User;
+//            var oldSession = await sessionRepository.GetById(request.SessionId);
+//            var authorizationResult = await authorizationService
+//            .AuthorizeAsync(user, oldSession, new UserIsHostRequirement(ResourceOperation.Delete));
+
+//            if (!authorizationResult.Succeeded)
+//            {
+//                throw new ForbidException();
+//            }
+
+
+//            var updatedSession = await sessionRepository.Update()
+//            return Unit.Value;
+//        }
+    
+//    }
+//}
