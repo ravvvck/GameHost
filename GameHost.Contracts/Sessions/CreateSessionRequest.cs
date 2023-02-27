72620ff53ef972f1a719537a6357a6ab90c83c3c
Rafał Kwiatek <rafal.kwiatek@edu.uekat.pl>
2023-02-27 22:10:27 +0100

    Add session contracts

--- GameHost.Contracts/Sessions/CreateSessionRequest.cs
+++ GameHost.Contracts/Sessions/CreateSessionRequest.cs
@@ -8,17 +8,32 @@ using System.Threading.Tasks;
 
 namespace GameHost.Contracts.Sessions
 {
-    public record CreateSessionRequest(
-    string Name,
-    string Description,
-    Address Address,
-    DateTime Date,
-    List<Game> Games){};
+    public class CreateSessionRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public DateTime Date { get; set; }
+        public List<Game> Games { get; set; }
+    }
 
-    public record Game(
-        string Name,
-        string Description,
-        string InfoURL
-       );
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string InfoURL { get; set; }
+    }
+    //public record CreateSessionRequest(
+    //string Name,
+    //string Description,
+    //Address Address,
+    //DateTime Date,
+    //List<Game> Games){};
+
+    //public record Game(
+    //    string Name,
+    //    string Description,
+    //    string InfoURL
+    //   );
    
 }
--- GameHost.Contracts/Sessions/SessionResponse.cs
+++ GameHost.Contracts/Sessions/SessionResponse.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GameHost.Domain.Common.ValueObjects;
+using GameHost.Domain.Hosts.ValueObjects;
+using GameHost.Domain.Sessions.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +9,37 @@ using System.Threading.Tasks;
 
 namespace GameHost.Contracts.Sessions
 {
-    public record SessionResponse(
-    Guid Id,
-    string HostId,
-    string Name,
-    string Description,
-    float? AverageRating,
-    List<GameResponse> Games,
-    DateTime Date,
-    List<string> SessionReviewIds);
+    public class SessionResponse
+    {
+        public SessionId SessionId { get; set; }
+        public HostId HostId { get; set; }
+        public Address Address { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool AlreadyHappend { get; set; }
+        public DateTime Date { get; set; }
+        public List<GameResponse> Games { get; set; }
+    }
 
-    public record GameResponse(
-        string Id,
-        string Name,
-        string Description,
-        string InfoURL);
+    public class GameResponse
+    {
+        public GameId GameId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string InfoURL { get; set; }
+    }
+    //public record SessionResponse(
+    //SessionId SessionId,
+    //HostId HostId,
+    //string Name,
+    //string Description,
+    //List<GameResponse> Games,
+    //DateTime Date);
+    //public record GameResponse(
+    //    GameId GameId,
+    //    string Name,
+    //    string Description,
+    //    string InfoURL);
         
     
 }
