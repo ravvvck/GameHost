5a417fc4932e2f29b78228670107cb242d8d7ae6
Rafał Kwiatek <rafal.kwiatek@edu.uekat.pl>
2023-02-27 22:08:26 +0100

    Add query for all sessions

---
+++ GameHost.Application/Sessions/Queries/GetAllSessionsQuery.cs
@@ -0,0 +1,14 @@
+﻿using GameHost.Domain.Sessions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHost.Application.Sessions.Queries
+{
+    public class GetAllSessionsQuery : IRequest<List<Session>>
+    {
+    }
+}
---
+++ GameHost.Application/Sessions/Queries/GetAllSessionsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using GameHost.Application.Common.Interfaces.Persistence;
+using GameHost.Domain.Sessions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHost.Application.Sessions.Queries
+{
+    public class GetAllSessionsQueryHandler : IRequestHandler<GetAllSessionsQuery, List<Session>>
+    {
+        private readonly ISessionRepository sessionRepository;
+
+        public GetAllSessionsQueryHandler(ISessionRepository sessionRepository)
+        {
+            this.sessionRepository = sessionRepository;
+        }
+        public async Task<List<Session>> Handle(GetAllSessionsQuery request, CancellationToken cancellationToken)
+        {
+            var sessions = await sessionRepository.GetAll();
+            return sessions;
+        }
+    }
+}
