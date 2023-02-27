70e3b31f3c1697421b91c77f5edbbe95a5419592
Rafał Kwiatek <rafal.kwiatek@edu.uekat.pl>
2023-02-27 22:02:06 +0100

    Add custom authorization handlers

---
+++ GameHost.Application/Authorization/UserIsHostRequirement.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHost.Application.Authentication.Authorization
+{
+   
+        public enum ResourceOperation
+        {
+            Create,
+            Read,
+            Update,
+            Delete
+        }
+        public class UserIsHostRequirement : IAuthorizationRequirement
+        {
+            public UserIsHostRequirement(ResourceOperation resourceOperation)
+            {
+                ResourceOperation = resourceOperation;
+            }
+
+            public ResourceOperation ResourceOperation { get; set; }
+        }
+}
+
---
+++ GameHost.Application/Authorization/UserIsHostRequirementHandler.cs
@@ -0,0 +1,35 @@
+﻿using GameHost.Application.Authentication.Authorization;
+using GameHost.Domain.Hosts.ValueObjects;
+using GameHost.Domain.Sessions;
+using GameHost.Domain.Users.ValueObjects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHost.Application.Authorization
+{
+
+    public class UserIsHostRequirementHandler : AuthorizationHandler<UserIsHostRequirement, Session>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserIsHostRequirement requirement, Session session)
+        {
+            if (requirement.ResourceOperation == ResourceOperation.Read || requirement.ResourceOperation == ResourceOperation.Create)
+            {
+                context.Succeed(requirement);
+            }
+            var sessionHostUserId = session.Host.User.Id;
+            var userId = UserId.Create(Guid.Parse(context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value));
+            if (sessionHostUserId.Value == userId.Value)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
